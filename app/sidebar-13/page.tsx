a3140664bb85cfaeae10ba01a4abba633f7b8703
wheza <wheza99@gmail.com>
2025-07-21 18:30:28 +0700

    grouping shadcn-blocks

--- app/sidebar-13/page.tsx
+++
@@ -1,9 +0,0 @@
-import { SettingsDialog } from "@/components/sidebar-13/settings-dialog"
-
-export default function Page() {
-  return (
-    <div className="flex h-svh items-center justify-center">
-      <SettingsDialog />
-    </div>
-  )
-}
